c2bf21e021240e001d69df96d85d67e918fcc7ff
yaoxinru <tt_yaoxinru@qq.com>
2022-03-31 20:51:17 +0800

    这是Text分支第一次提交

--- src/main/java/cn/ys/Controller/TextController.java
+++ src/main/java/cn/ys/Controller/TextController.java
@@ -3,6 +3,7 @@ package cn.ys.Controller;
 public class TextController {
     public static  void  main(String [] args){
         System.out.println("hello  git!");
+        System.out.println("hello git text!");
 
     }
 }
