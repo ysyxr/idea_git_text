39ad428af0601d0fda3310c58c0fc487bee4b962
yaoxinru <tt_yaoxinru@qq.com>
2022-03-31 20:46:16 +0800

    这是dev第一次提交

--- src/main/java/cn/ys/Controller/TextController.java
+++ src/main/java/cn/ys/Controller/TextController.java
@@ -3,6 +3,8 @@ package cn.ys.Controller;
 public class TextController {
     public static  void  main(String [] args){
         System.out.println("hello  git!");
+        System.out.println("hello  git  dev!");
+
 
     }
 }
