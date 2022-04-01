e66e962330366f9cfe26823e599d709246c4f6f8
yaoxinru <tt_yaoxinru@qq.com>
2022-04-01 15:01:45 +0800

    这是Text分支第三次提交

--- src/main/java/cn/ys/Controller/TextController.java
+++ src/main/java/cn/ys/Controller/TextController.java
@@ -4,8 +4,8 @@ public class TextController {
     public static  void  main(String [] args){
         System.out.println("hello  git!");
         System.out.println("hello  git  dev!");
-
         System.out.println("hello git text!");
+        System.out.println("这是gitub提交");
 
     }
 }
